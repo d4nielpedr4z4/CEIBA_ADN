ddbd31aaacfef02e4b6144d9026dbaff5950112f
Daniel Andrés Pedraza Romero <daniel.pedraza@ceiba.com.co>
2022-05-18 13:16:06 -0500

    Refactor modelo de dominio

--- microservicio/dominio/src/test/java/com/ceiba/producto/servicio/ServicioEliminarProductoTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/producto/servicio/ServicioEliminarProductoTest.java
@@ -1,6 +1,10 @@
 package com.ceiba.producto.servicio;
 
+import com.ceiba.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.producto.modelo.entidad.Producto;
 import com.ceiba.producto.puerto.repositorio.RepositorioProducto;
+import com.ceiba.producto.servicio.testdatabuilder.ProductoTestDataBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -15,6 +19,19 @@ public class ServicioEliminarProductoTest {
         Mockito.when(repositorioProducto.existePorId(Mockito.anyLong())).thenReturn(true);
         servicioEliminarProducto.ejecutar(10l);
         Mockito.verify(repositorioProducto, Mockito.times(1)).eliminar(10l);
+    }
+
+    @Test
+    @DisplayName("Deberia lanzar una exepcion cuando no existe el Producto")
+    void deberiaLanzarUnaExepcionCuandoNoExisteElProducto() {
+        // arrange
+        Producto producto = new ProductoTestDataBuilder().build();
+        RepositorioProducto repositorioProducto = Mockito.mock(RepositorioProducto.class);
+        Mockito.when(repositorioProducto.existePorId(Mockito.anyLong())).thenReturn(false);
+        ServicioEliminarProducto servicioEliminarProducto = new ServicioEliminarProducto(repositorioProducto);
+        // act - assert
+        BasePrueba.assertThrows(() -> servicioEliminarProducto.ejecutar(producto.getId()), ExcepcionDuplicidad.class,
+                "El producto no se encuentra registrado");
     }
 
 }
